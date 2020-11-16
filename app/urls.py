22543cf73f3c831eff9d491df4d371b9971dac98
Swayam <71008174+Swayam600@users.noreply.github.com>
2020-11-16 12:31:19 +0530

    Added the method to redirect the user to the home page

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='redirected_homepage'),
     path('home/', views.home, name = 'home-page'),
     path('add_task/', views.add_task, name = 'add_task'),
     path('delete_task/<int:task_id>/', views.delete_task, name = 'delete_task'),
